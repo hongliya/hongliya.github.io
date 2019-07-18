668718a061379e6545b8a617ce845188602c031e
CY-20180503JGQG\Administrator <15817410093@louxun.com>
2019-07-18 15:07:37 +0800

    百度统计

---
+++ works/js/bdtj.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9f6c79c25d6b6f07d68d1ec0e7847067";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
